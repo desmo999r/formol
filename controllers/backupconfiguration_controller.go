9ed45d852827ae65cf4ef4e55052714fc259c762
Jean-Marc ANDRE <jeanmarc.jim.andre@gmail.com>
2023-02-04 18:11:30 +0100

    Reorganized files one more time

--- controllers/backupconfiguration_controller.go
+++ controllers/backupconfiguration_controller.go
@@ -28,7 +28,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	formolv1alpha1 "github.com/desmo999r/formol/api/v1alpha1"
-	//formolutils "github.com/desmo999r/formol/pkg/utils"
 )
 
 // BackupConfigurationReconciler reconciles a BackupConfiguration object
@@ -43,15 +42,6 @@ type BackupConfigurationReconciler struct {
 //+kubebuilder:rbac:groups=formol.desmojim.fr,resources=backupconfigurations/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=formol.desmojim.fr,resources=backupconfigurations/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the BackupConfiguration object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.1/pkg/reconcile
 func (r *BackupConfigurationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	r.Context = ctx
 	r.Log = log.FromContext(ctx)
